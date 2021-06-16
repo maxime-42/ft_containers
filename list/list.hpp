d17e14506521512bb89801c2fe2b7aa2aa63921b
maxime-42 <mkayumba@student.42.fr>
2021-06-16 19:15:07 +0200

    debug sort in progress

--- list/list.hpp
+++ list/list.hpp
@@ -41,14 +41,20 @@ namespace ft
 		list (const allocator_type& alloc = allocator_type()):_head(0), _size(0)	
 		{
 			_head = _alloc_node.allocate(1);
-			_myAlloc.construct(&_head->data, 555);
+			// _myAlloc.construct(&_head->data, 555);
 			_head->next = _head;
 			_head->prev = _head;
 			 (void)alloc;
 		}
 
-		list (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type())
+		list (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()):_head(0), _size(0)	
 		{
+			(void)alloc;
+			_head = _alloc_node.allocate(1);
+			_myAlloc.construct(&_head->data, 555);
+			_head->next = _head;
+			_head->prev = _head;
+			std::cout<< "hellow world\n";
 			while (n > 0)
 			{
 				this->push_back(val);
@@ -57,12 +63,16 @@ namespace ft
 		}
 		list (const list& x)
 		{
-				this->push_back(x);
+			_head = _alloc_node.allocate(1);
+			_head->prev = _head;
+			_head->next = _head;
+			this->push_back(x);
 		}
-
-		template <typename T, typename Alloc> template <class InputIterator>
-		list<T, Alloc>::list(typename std::enable_if<!std::numeric_limits<InputIterator>::is_integer, InputIterator>::type first, InputIterator last, const allocator_type& alloc) :_head(0), _size(0)
+		
+		template <class InputIterator>
+		list(typename std::enable_if<!std::numeric_limits<InputIterator>::is_integer, InputIterator>::type first, InputIterator last, const allocator_type& alloc) :_head(0), _size(0)
 		{
+			(void)alloc;
 			_head = _alloc_node.allocate(1);
 			_head->prev = _head;
 			_head->next = _head;
@@ -72,19 +82,35 @@ namespace ft
         		first++;
     		}
 		}
-		void delete_one_Node()
+
+		void delete_one_Node(t_node *toDel)
 		{
-			
+			t_node *prev = toDel->prev;
+			t_node *next = toDel->next;
+
+			prev->next = next;
+			next->prev = prev;
+			toDel->next = 0;
+			toDel->prev = 0;
+			_alloc_node.deallocate(toDel, 1);
+			_alloc_node.destroy(toDel);
+			toDel = 0;
 		}
 		~list()
 		{
-
+			size_type count = _size;
+			for (size_t i = 0; i < count; i++)
+			{
+				this->delete_one_Node(_head->next);
+			}
+			_head = 0;
+			_size = 0;
 		}
 /************************************************************************  iterator ************************************************************************/
 		class  iterator 
 		{
 		public:
-		    typedef t_node *                           pointeur;
+		    typedef t_node *                           pointer_node;
 
 		/***************Is default-constructible, copy-constructible, copy-assignable and destructible	******************/
 			iterator():m_ptr(0){}
@@ -130,7 +156,7 @@ namespace ft
 			friend bool operator>=(const  iterator& a, const   iterator& b) { return (a.m_ptr >= b.m_ptr); };
 
 		private:
-			pointeur m_ptr;
+			pointer_node m_ptr;
 		};
 		iterator begin() {return   iterator(_head->next); }
 		iterator end()   { return  iterator(_head); }
@@ -158,7 +184,63 @@ namespace ft
         }
 
 		size_type	size(void){return (_size);}
-		
+
+		template <class M>
+		void ft_swap(M &a, M &b)
+		{
+			M tmp = a;
+			a = b;
+			b = tmp;
+		}
+
+		void swap (list& x)
+		{
+			ft_swap(_head, x._head);
+			ft_swap(_size, x._size);
+			ft_swap(_myAlloc, x._myAlloc);
+			ft_swap(_alloc_node, x._alloc_node);
+		}
+		void	swap_node(t_node *a, t_node *b)
+		{
+			t_node *tmpNextB = b->next;
+			b->next =b->prev;
+			b->prev = a->prev;
+			a->next = tmpNextB;
+			a->next->prev = a;
+		}
+		void	updateHead(t_node *a, t_node *b)
+		{
+			if (_head->prev == b)
+			{
+				
+					  std::cout << "a = " << a->data << "\n";
+				_head->prev = a;
+			}
+			// else if (_head->prev == b)
+			// {
+			// 	_head->prev = b;
+			// }
+		}
+
+		void sort()
+		{
+			if (_size < 2)
+				return ;
+			t_node *it = _head->next;
+			for (size_type i = 0; i < (_size - 1); i++)
+			{
+				if (it->data > it->next->data)
+				{
+					  std::cout << "sort\n";
+					updateHead(it->next, it);
+					return ;
+					// swap_node(it, it->next);
+
+					i = 0;
+				}
+			}
+			
+		}
 	};
 }
 
--- list/main.cpp
+++ list/main.cpp
@@ -23,6 +23,7 @@ void test_push_front()
         std::cout << " " << *it << std::endl;
     }
 }
+
 void test_push_back()
 {
   ft::list<int> myList;
@@ -44,10 +45,86 @@ void test_push_back()
     }
 }
 
+void test_swap()
+{
+  ft::list<int> first (5,100);   // three ints with a value of 100
+//   ft::list<int> second (5,200);  // five ints with a value of 200
+// 
+//   first.swap(second);
+
+  std::cout << "first contains:";
+  for (ft::list<int>::iterator it=first.begin(); it!=first.end(); it--)
+    std::cout << ' ' << *it;
+  std::cout << '\n';
+// // 
+//   std::cout << "second contains:";
+//   for (ft::list<int>::iterator it=second.begin(); it!=second.end(); it++)
+//     std::cout << ' ' << *it;
+//   std::cout << '\n';
+}
+			// _myAlloc.deallocate(toDel, 1);
+void test_sort()
+{
+  std::list<int> mylist;
+  std::list<int>::iterator it;
+  mylist.push_back (5);
+  mylist.push_back (1);
+  mylist.push_back (9);
+  mylist.push_back (2);
+
+
+  std::cout << "befor sort mylist contains:\n";
+  for (it=mylist.begin(); it!=mylist.end(); ++it)
+    std::cout << *it << " addr = " << &*it << "\n";
+  std::cout << "\n\n";
+
+  mylist.sort();
+
+//   mylist.sort(compare_nocase);
+
+  std::cout << "after sor  mylist contains:\n";
+  for (it=mylist.begin(); it!=mylist.end(); ++it)
+    std::cout << *it << " addr = " << &*it<< "\n";
+  std::cout << '\n';
+}
+
+void my_test_sort()
+{
+  ft::list<int> mylist;
+  ft::list<int>::iterator it;
+  mylist.push_back (5);
+  mylist.push_back (1);
+//   mylist.push_back (9);
+//   mylist.push_back (2);
+
+
+  std::cout << "befor sort mylist contains:\n";
+  for (it=mylist.begin(); it!=mylist.end(); it++)
+    std::cout << *it << " addr = " << &*it << "\n";
+  std::cout << "\n\n";
+
+  mylist.sort();
+    
+    it = mylist.begin();it--;
+  std::cout << "begin =  " << *it;
+
+    // it = mylist.end();
+//     it++;
+//   std::cout << "\nend =  " << *it;
+    return ;
+  std::cout << "after sor  mylist contains:\n";
+  for (it=mylist.begin(); it!=mylist.end(); it++)
+    std::cout << *it << " addr = " << &*it<< "\n";
+  std::cout << '\n';
+}
+
+
 int main(void) 
 {
-    test_push_front();
-    std::cout << "\n\n" << std::endl;
+    // test_push_front();
+    // test_swap();
+    my_test_sort();
+    // std::cout << "\n\n" << std::endl;
     // test_push_back();
   
     return (0);
