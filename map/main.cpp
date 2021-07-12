74716c9df7d6e2c9c9f3aea43d2b15705ee76cae
Maxime KAYUMBA ONAKASONGO <mkayumba@e2r9p18.42.fr>
2021-07-12 17:50:53 +0200

    /map.hpp:26:12: error: cannot assign to non-static data member 'first' with const-qualified type 'const char'

--- map/main.cpp
+++ map/main.cpp
@@ -1,20 +1,42 @@
 #include "map.hpp"
 #include <map>
 
-int  main(void) 
+// int	main(void) 
+// {
+// 	// ft::map<int, int> obj;
+// 	// ft::mappair<int,int> p(1,100);
+// 	// obj.insert();
+// 	std::map<char,int> mymap;
+// 	std::pair<char,int> p('a',100);
+// 	// first insert function version (single parameter):
+// 	mymap.insert (p);
+// 	// mymap.insert ( std::pair<char,int>('b',200) );
+// 	// mymap.insert ( std::pair<char,int>('c',400) );
+// 	// mymap.insert ( std::pair<char,int>('d',400) );
+
+
+// 	// std::cout << "size = " << mymap.size() << std::endl;
+// 	for (std::map<char, int>::iterator it=mymap.begin(); it!=mymap.end(); ++it)
+// 		std::cout << it->first << " => " << it->second << '\n';
+// 	return (0);
+// }
+
+int	main(void)
 {
 	// ft::map<int, int> obj;
-	std::map<char,int> mymap;
-
+	// ft::mappair<int,int> p(1,100);
+	// obj.insert();
+	ft::map<char, int> mymap;
+	ft::pair<char,int> p('a',100);
+	// ft::pair<char,int> p1(p);
+	// std::cout << "first " << p1.second << std::endl;
 	// first insert function version (single parameter):
-	mymap.insert ( std::pair<char,int>('a',100) );
-	mymap.insert ( std::pair<char,int>('b',200) );
-	mymap.insert ( std::pair<char,int>('c',400) );
-	mymap.insert ( std::pair<char,int>('d',400) );
-
-
-	std::cout << "size = " << mymap.size() << std::endl;
-	for (std::map<char, int>::iterator it=mymap.begin(); it!=mymap.end(); ++it)
-	std::cout << it->first << " => " << it->second << '\n';
+		mymap.insert (p);
+	// mymap.insert ( std::pair<char,int>('b',200) );
+	// mymap.insert ( std::pair<char,int>('c',400) );
+	// mymap.insert ( std::pair<char,int>('d',400) );
+	// std::cout << "size = " << mymap.size() << std::endl;
+	// for (std::map<char, int>::iterator it=mymap.begin(); it!=mymap.end(); ++it)
+		// std::cout << it->first << " => " << it->second << '\n';
 	return (0);
 }
--- map/map.hpp
+++ map/map.hpp
@@ -7,23 +7,27 @@ namespace ft
 	template <typename T1, typename T2> 
 	class pair
 	{
-	public:
-	// ******** Data ********
-		T1	first;
-		T2	second;
-	// ******** Member function ********
-		pair() : first(), second() { };
-		template<class U, class V>
-		pair (const pair<U, V> &pr) : first(pr.first), second(pr.second) { };
-		pair (const T1& a, const T2& b) : first(a), second(b) { };
-		~pair() { };
-		pair& operator= (const pair& pr)
-		{
-			if (this != &pr)
-			first = pr.first;
-			second = pr.second;
-			return (*this);
-		}
+		public:
+		// ******** Data ********
+			T1	first;
+			T2	second;
+		// ******** Member function ********
+			pair() : first(T1()), second(T2()) { };
+			template<class U, class V>
+			pair (const pair<U, V> &pr) : first(pr.first), second(pr.second) { };
+			pair (const T1& a, const T2& b) : first(a), second(b) { };
+			pair (T1& a, T2& b) : first(a), second(b) { };
+
+			~pair() {};
+			pair& operator=(const pair & pr)
+			{
+				if (this != &pr)
+				{
+					first = pr.first;
+					second = pr.second;
+				}
+				return (*this);
+			}
 	};
 
 	template <	class Key,                                        // map::key_type
@@ -34,32 +38,84 @@ namespace ft
 
 	class map
 	{
-	public:
-		typedef pair<const Key, T>					value_type;
-		typedef T& 									reference;
-		typedef Alloc								allocator_type;
-		typedef const T&							const_reference;
-		typedef T* 									pointer;
-		typedef T* const 							const_pointer;
-		typedef std::ptrdiff_t						difference_type;
-		typedef size_t								size_type;
-		typedef std::forward_iterator_tag			iterator_category;
-		
-		struct										s_struct
-		{
-			value_type									data;
-			struct s_struct							*left;
-			struct s_struct							*right;
-		}											t_struct;
-		map()
-		{
-			std::cout << "hellow world\n" << std::endl;
-		}
-		
-		~map()
-		{
-			std::cout << "destructeur" << std::endl;
-		}
+		public:
+			typedef pair<const Key, T>					value_type;
+			typedef T& 									reference;
+			typedef Alloc								allocator_type;
+			typedef const T&							const_reference;
+			typedef T* 									pointer;
+			typedef T* const 							const_pointer;
+			typedef std::ptrdiff_t						difference_type;
+			typedef size_t								size_type;
+			typedef std::forward_iterator_tag			iterator_category;
+
+			typedef struct								s_node
+			{
+				value_type								data;
+				struct s_node							*left;
+				struct s_node							*right;
+			}											t_node;
+		protected:
+			t_node										*_root;
+			size_type									_size;
+			std::allocator<struct s_node>				_alloc_node;
+			Alloc										_myAlloc;
+		public:
+		///////////////////////iterator/////////////////////////////////
+ 			class iterator
+			{
+				public:
+					typedef t_node						*pointeur;
+					// typedef ptrdiff_t					difference_type;
+
+				iterator(pointeur ptr = 0): _ptr(ptr){}
+				// iterator(iterator const &cp){_ptr = cp.get_ptr();}
+				// iterator operator=(MapIterator const &cp)
+				// {
+					// if (this != &cp)
+						// this->_ptr = cp.get_ptr();
+					// return *this;
+				// }
+
+				~iterator(){}
+				value_type		&operator*()const {return _ptr->data;}
+				value_type		*operator->()const {return &(_ptr->data);}
+				// MapIterator	&operator++(){_ptr = _ptr->getnext(); return *this;}
+				// MapIterator	operator++(int){iterator it = *this; _ptr = _ptr->getnext(); return it;}
+				// MapIterator	&operator--(){ _ptr = _ptr->getprev(); return *this;}
+				// MapIterator	operator--(int){iterator it = *this; _ptr = _ptr->getprev(); return it;}
+				// pointeur        get_ptr()const{return _ptr;}
+
+				// bool			operator==(const iterator &it){ return _ptr == it.get_ptr();}
+				// bool			operator!=(const iterator &it){ return _ptr != it.get_ptr();}
+
+				private:
+					pointeur							_ptr; 
+			};
+			
+			map():_root(0)
+			{
+				std::cout << "hellow world\n" << std::endl;
+			}
+
+			~map()
+			{
+				std::cout << "destructeur" << std::endl;
+			}
+
+			void insert (const value_type & val)
+			{
+				(void)val;
+				if (!_root)
+				{
+					_root = _alloc_node.allocate(1);
+					_root->data = val;
+					// _root->data.first = val.first;
+					// std::cout << "val first" << val.first << std::endl;
+					// _myAlloc.construct(&root, val);
+				}
+			}
+
 	};
 }
 
