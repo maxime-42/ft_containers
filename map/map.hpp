b7f307030fa8a297d6ae8ba8ba35bb47a0f8c0a9
Maxime KAYUMBA ONAKASONGO <mkayumba@e2r6p18.42.fr>
2021-07-14 19:47:24 +0200

    add get next node

--- map/map.hpp
+++ map/map.hpp
@@ -44,7 +44,7 @@ namespace ft
 			typedef Compare								key_compare;
 			typedef Alloc								allocator_type;
 			typedef const T&							const_reference;
-			typedef T* 									pointer;
+			typedef value_type*							pointer;
 			typedef T* const 							const_pointer;
 			typedef std::ptrdiff_t						difference_type;
 			typedef size_t								size_type;
@@ -65,69 +65,103 @@ namespace ft
 		///////////////////////iterator/////////////////////////////////
  			class iterator
 			{
+				private:
+					t_node								_ptr; 
 				public:
 					typedef t_node						*pointeur;
 					// typedef ptrdiff_t					difference_type;
 
-				iterator(pointeur ptr = 0): _ptr(ptr){}
-				// iterator(iterator const &cp){_ptr = cp.get_ptr();}
-				// iterator operator=(MapIterator const &cp)
-				// {
-					// if (this != &cp)
-						// this->_ptr = cp.get_ptr();
-					// return *this;
-				// }
+					iterator(pointeur ptr = 0): _ptr(ptr){}
+					// iterator(iterator const &cp){_ptr = cp.get_ptr();}
+					// iterator operator=(MapIterator const &cp)
+					// {
+						// if (this != &cp)
+							// this->_ptr = cp.get_ptr();
+						// return *this;
+					// }
+					~iterator(){}
+					value_type		&operator*()const {return _ptr->data;}
+					value_type		*operator->()const {return &(_ptr->data);}
+					// MapIterator	&operator++(){_ptr = _ptr->getnext(); return *this;}
+					// MapIterator	operator++(int){iterator it = *this; _ptr = _ptr->getnext(); return it;}
+					// MapIterator	&operator--(){ _ptr = _ptr->getprev(); return *this;}
+					// MapIterator	operator--(int){iterator it = *this; _ptr = _ptr->getprev(); return it;}
+					// pointeur        get_ptr()const{return _ptr;}
 
-				~iterator(){}
-				value_type		&operator*()const {return _ptr->data;}
-				value_type		*operator->()const {return &(_ptr->data);}
-				// MapIterator	&operator++(){_ptr = _ptr->getnext(); return *this;}
-				// MapIterator	operator++(int){iterator it = *this; _ptr = _ptr->getnext(); return it;}
-				// MapIterator	&operator--(){ _ptr = _ptr->getprev(); return *this;}
-				// MapIterator	operator--(int){iterator it = *this; _ptr = _ptr->getprev(); return it;}
-				// pointeur        get_ptr()const{return _ptr;}
+					// bool			operator==(const iterator &it){ return _ptr == it.get_ptr();}
+					// bool			operator!=(const iterator &it){ return _ptr != it.get_ptr();}
 
-				// bool			operator==(const iterator &it){ return _ptr == it.get_ptr();}
-				// bool			operator!=(const iterator &it){ return _ptr != it.get_ptr();}
-
-				private:
-					pointeur							_ptr; 
 			};
-			
-			map():_root(0)
+			t_node *get_begin(t_node *node)
+			{
+				if (_node)
+				{
+					get_begin(node->left);
+					return (node);
+				}
+				return (0);
+			}
+
+			t_node *get_next_node(t_node *node)
+			{
+				if (node)
+				{
+					get_next_node(node->left);
+					return (node);
+				}
+				return (0);
+			}
+
+			iterator begin()
+			{
+				return (get_begin(_root));
+			}
+
+			map():_root(0), _size(0)
 			{
 				std::cout << "hellow world\n" << std::endl;
 			}
 
 			~map()
 			{
+				print_tree(_root);
 				std::cout << "destructeur" << std::endl;
 			}
 
-			void my_insert (const value_type & val, t_node *node)
+ 			void my_insert (const value_type & val, t_node **node, key_compare cmp)
 			{
-				if (!node)
+				if (!*node)
 				{
 					_size++;
-					_root = _alloc_node.allocate(1);
-					_myAlloc.construct(&node->data, val);
+					*node = _alloc_node.allocate(1);
+					_myAlloc.construct(&(*node)->data, val);
 				}
-				if (key_compare(node->data.first, val.first))
+				else if (cmp((*node)->data.first, val.first))
 				{
-					my_insert(val, node->left);
+					my_insert(val, &(*node)->right, cmp);
 				}
-				// else if (!key_compare(node->data.first, val.first))
-				// {
-				// 	my_insert(val, node->right);
-				// }
-				// if (!(root->data.first ^ val.first))
-					// return ;
+				else if (cmp((*node)->data.first, val.first) == false)
+				{
+					if (cmp(val.first, (*node)->data.first) == false)
+						return ;
+					my_insert(val, &(*node)->left, cmp);
+				}
 			}
 
 			void insert (const value_type & val)
 			{
 				(void)val;
-				my_insert (val, _root);
+				my_insert (val, &_root, key_compare());
+			}
+
+			void	print_tree(t_node *node)
+			{
+				if (node)
+				{
+					print_tree(node->left);
+					std::cout << "first = " << node->data.first << " second = " << node->data.second << "\n" ;
+					print_tree(node->right);
+				}
 			}
 
 	};
